79cadfb60a20e2629507f68e12df88a01affa3ab
Araujo <86306571+PhelipeCurso@users.noreply.github.com>
2022-10-14 17:16:18 -0300

    v004

---
+++ LocacaoDeVeiculos.DATA/Services/VeiculoService.cs
@@ -0,0 +1,20 @@
+﻿using LocacaoDeVeiculos.DATA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoDeVeiculos.DATA.Services
+{
+    public class VeiculoService
+    {
+        public RepositoryVeiculo oRepositoryVeiculo { get; set; }    
+
+        public VeiculoService()
+        {
+            oRepositoryVeiculo = new RepositoryVeiculo();
+        }
+
+    }
+}
---
+++ WSBeta.WEB/Controllers/VeiculoController.cs
@@ -0,0 +1,58 @@
+﻿using LocacaoDeVeiculos.DATA.Models;
+using LocacaoDeVeiculos.DATA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WSBeta.WEB.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        private VeiculoService oVeiculoService = new VeiculoService();
+        public IActionResult Index()
+        {
+            List<Veiculo> oListVeiculos = oVeiculoService.oRepositoryVeiculo.SelecionarTodos();
+            return View(oListVeiculos);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Veiculo model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            oVeiculoService.oRepositoryVeiculo.Incluir(model);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+           Veiculo oVeiculo = oVeiculoService.oRepositoryVeiculo.SelecionarPk(id);
+            return View(oVeiculo);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Veiculo oVeiculo = oVeiculoService.oRepositoryVeiculo.SelecionarPk(id);
+            return View(oVeiculo);
+        }
+        [HttpPost]
+        public IActionResult Edit(Veiculo model)
+        {
+            Veiculo oVeiculo = oVeiculoService.oRepositoryVeiculo.Alterar(model);
+
+            int id = oVeiculo.Id;
+
+            return RedirectToAction("Details", new { id });
+        }
+        public IActionResult Delete(int id)
+        {
+            oVeiculoService.oRepositoryVeiculo.Excluir(id);
+            return RedirectToAction("index");
+        }
+    }
+}
